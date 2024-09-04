98cf4176d466386504a55be0d5488c0f07672674
Yazan Egbaria <yazanegbaria07@gmail.com>
2024-09-04 09:48:26 +0300

    Added primitive types tasks

---
+++ index.js
@@ -0,0 +1,28 @@
+// String
+const fullName = "Yazan Egbaria";
+
+console.log(fullName);
+
+// Numbers
+const num1 = 7;
+const num2 = 3;
+
+console.log(num1 + num2);
+
+// Booleans
+
+const isWorking = true;
+
+console.log(isWorking);
+
+// Undefiend
+
+let i;
+
+console.log(i);
+
+// Null
+
+let y = null;
+
+console.log(y);
