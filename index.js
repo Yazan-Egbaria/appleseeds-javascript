b98adee6a5df6beb9db5bda433397dd6a4cfa844
Yazan Egbaria <yazanegbaria07@gmail.com>
2024-09-04 14:57:14 +0300

    Added string concatenation exercise

---
+++ index.js
@@ -0,0 +1,26 @@
+// Introduce yourself
+
+const name = "Yazan";
+const age = 23;
+console.log("My name is " + name + " and I am " + age + " years old");
+
+// Favorite Things
+
+const favColor = "red";
+const favMovie = "Avengers";
+const favFood = "Burger";
+console.log(
+  `My favorite color is ${favColor} and my favorite movie is ${favMovie} and my favorite food is ${favFood}`
+);
+
+// Simple Math
+
+const a = 7;
+const b = 3;
+const result = a + b;
+console.log(`The sum of ${a} and ${b} is ${result}`);
+
+// Day of the week
+
+const weekDay = "Wednesday";
+console.log("Today is " + weekDay);
