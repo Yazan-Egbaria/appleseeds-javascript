5e40193073e35a148e030aef2dc94b60f5a8424e
Yazan Egbaria <yazanegbaria07@gmail.com>
2024-09-04 14:10:28 +0300

    Modified version

---
+++ index.js
@@ -0,0 +1,33 @@
+// Type Identification
+
+const num = 10;
+const str = "Hello";
+const bool = true;
+console.log(typeof num);
+console.log(typeof str);
+console.log(typeof bool);
+
+// Comparing Variable Types
+
+const firstVar = 42;
+const secondVar = "42";
+console.log(typeof firstVar);
+console.log(typeof secondVar);
+
+// Undefined and Null Types
+
+let noValue;
+let emptyValute = null;
+console.log(typeof noValue);
+console.log(typeof emptyValute);
+
+// Debugging Variables
+
+const username = "Yazan Egbaria";
+const age = 23;
+const isWorking = true;
+console.log(
+  `The value of username is ${username} and the type of username is ${typeof username}, 
+  The value of age is ${age} and the type of age is ${typeof age},
+  The Value of isWorking is ${isWorking} and the type of isWorking is ${typeof isWorking}`
+);
