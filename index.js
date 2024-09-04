91021d84dd005eb34a418cb88ee86f0997641e96
Yazan Egbaria <yazanegbaria07@gmail.com>
2024-09-04 13:45:37 +0300

    Added values and variables task

---
+++ index.js
@@ -0,0 +1,26 @@
+var num1 = 7;
+
+let username1 = "Yazan Egbaria";
+
+const gotChildren = true;
+
+var num2 = 10;
+console.log(num2);
+num2 = 7;
+console.log(num2);
+
+let username2 = "Noor Athamny";
+console.log(username2);
+username2 = "Mohamed Egbaria";
+console.log(username2);
+
+const gotCar = true;
+console.log(gotCar);
+gotCar = false;
+console.log(gotCar);
+
+/* 
+Got an error because u cannot reassign the value of a constant variable!
+If u want to reassign a value of a variable it is recommended to use let or var (mostly let)
+Other than that const is recommended for static values (long term values that do not change)
+*/
