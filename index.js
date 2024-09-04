9e4b3f95c7b400a2e786d329591f17c16198c6cb
Yazan Egbaria <yazanegbaria07@gmail.com>
2024-09-04 13:34:25 +0300

    Added naming exercise

---
+++ index.js
@@ -0,0 +1,15 @@
+// Current City Temperature
+
+let curParisTemp = 21;
+
+// User Login Status
+
+let isUserLoggedIn = true;
+
+// Customer's Email Address
+
+let formUserEmail = "yazan@gmail.com";
+
+// Basketball Total Score
+
+let basketballTotalScore = 91;
