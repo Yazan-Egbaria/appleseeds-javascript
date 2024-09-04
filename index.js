0f8b2032a4a9bbaac42dea555915a657830ae880
Yazan Egbaria <yazanegbaria07@gmail.com>
2024-09-04 13:59:46 +0300

    Added complex expression task

---
+++ index.js
@@ -0,0 +1,36 @@
+// Personal Greeting
+
+const name = "Yazan";
+const age = 23;
+console.log(`my name is ${name} and I am ${age} years old.`);
+
+// Calculating Area
+
+const length = 5;
+const width = 3;
+const area = length * width;
+console.log(
+  `The area of a rectangle with length of ${length} and width of ${width} is ${area}`
+);
+
+// Daily Schedule
+
+const today = "wednesday";
+const taskOne = "solve appleseeds exercises";
+const taskTwo = "go to the gym and workout";
+const taskThree = "go to my friends party";
+console.log(`On ${today} I will ${taskOne} and ${taskTwo} and ${taskThree}`);
+
+// Numbers and Strings
+
+const petsNum = 0;
+console.log(`I have ${petsNum} pets`);
+
+// Complex Expression
+
+const triangleBase = 8;
+const triangleHeight = 7;
+const triangleArea = triangleBase * triangleHeight;
+console.log(
+  `The area of a triangle with base ${triangleBase} and height ${triangleHeight} is ${triangleArea}`
+);
