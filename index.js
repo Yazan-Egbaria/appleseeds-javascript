3096958554b6ff5c4c65e97b335835023e5baa7b
Yazan Egbaria <yazanegbaria07@gmail.com>
2024-09-04 09:32:16 +0300

    Added comments task

---
+++ index.js
@@ -0,0 +1,9 @@
+// let array1 = [apple, oranges, bananas];
+
+/*
+let array2 = [apple, oranges, bananas];
+let array3 = [apple, oranges, bananas];
+let array4 = [apple, oranges, bananas];
+let array5 = [apple, oranges, bananas];
+let array6 = [apple, oranges, bananas];
+*/
