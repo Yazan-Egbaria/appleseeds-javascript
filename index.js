a3a98dce0cafc5b85ab6597b93c02a49b9955bdc
Yazan Egbaria <yazanegbaria07@gmail.com>
2024-09-05 13:37:08 +0300

    added boolean expressions exercise

---
+++ index.js
@@ -0,0 +1,61 @@
+// True or False
+
+const isSunny = true;
+const isWeekend = false;
+console.log(isSunny, isWeekend);
+
+// Checking Conditions
+
+const age = 20;
+const license = true;
+console.log(age > 18 ? license : !license);
+console.log(age > 18 && license);
+
+// Either OR Scenario
+
+const knowsJavascript = false;
+const knowsPython = true;
+console.log(knowsJavascript || knowsPython);
+
+// Negating a Boolean
+
+const isRaining = true;
+console.log(!isRaining);
+
+// Complex Logical Expression
+
+const likesMusic = true;
+const playsGuitar = true;
+const hasTime = false;
+console.log((likesMusic && playsGuitar) || hasTime);
+
+// Age Comparison
+
+const personAge = 23;
+console.log(personAge > 18);
+
+// Temprature Check
+
+const currentTemp = 30;
+console.log(currentTemp < 20 || currentTemp > 30);
+
+// Equality Check
+
+const firstNum = 10;
+const secondNum = "10";
+console.log(firstNum == secondNum);
+console.log(firstNum === secondNum);
+
+// Budget Limit
+
+const budget = 500;
+const expense = 450;
+console.log(expense <= budget);
+
+// Height Comparison
+
+const personOneHeight = 174;
+const personTwoHeight = 165;
+console.log(personOneHeight > personTwoHeight);
+console.log(personOneHeight < personTwoHeight);
+console.log(personOneHeight === personTwoHeight);
